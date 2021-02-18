409d06f6a7bc8fc2a31803132bd7c7631941d487
Lucas Bercê de Jesus <41213419+LucasBerce71@users.noreply.github.com>
2021-02-18 00:52:43 -0300

    feat: adding a dynamic imports example

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,6 +10,12 @@ interface HomeProps {
 }
 
 export default function Home({ recommendedProducts }: HomeProps) {
+  async function handleSum() {
+    const math = ( await import('../lib/math')).default;
+
+    alert(math.sum(3, 5));
+  }
+
   return (
     <div>
       <section>
@@ -24,6 +30,7 @@ export default function Home({ recommendedProducts }: HomeProps) {
           })}
         </ul>
       </section>
+      <button onClick={handleSum}>Sum!</button>
     </div>
   );
 }
